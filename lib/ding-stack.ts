1acbe7792d354a108135cc818bd8aae49675fa19
Stephen McGuckin <stephen.mcguckin@nordicsemi.no>
2022-12-02 21:08:55 -0800

    adds invite stack

--- lib/ding-stack.ts
+++ lib/ding-stack.ts
@@ -1,8 +1,39 @@
 import {Stack, StackProps} from 'aws-cdk-lib';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Topic } from 'aws-cdk-lib/aws-sns';
 import {Construct} from 'constructs';
 
-export class DingStack extends Stack {
+
+
+export class DingStack extends Construct {
     constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props);
+        super(scope, id);
+        new InvitesStack(scope, 'InvitesStack', props);
     }
+}
+export class InvitesStack extends Stack {
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);       
+        this.invitesTable();
+        this.invitesCreatedTopic();
+        this.createInviteFunction();
+    }
+
+    private readonly invitesTable = () => new Table(this, 'InvitesTable', {
+        partitionKey: {
+            name: 'id',
+            type: AttributeType.STRING
+        },
+        tableName: 'InvitesTable',
+    });
+
+    private readonly invitesCreatedTopic = () => new Topic(this, 'SnsTopic');
+
+    private readonly createInviteFunction = () => new NodejsFunction(this, 'InviteHandler', {
+        entry: 'src/invite-create.ts',
+        handler: 'inviteCreate',
+        runtime: Runtime.NODEJS_16_X
+    });
 }
